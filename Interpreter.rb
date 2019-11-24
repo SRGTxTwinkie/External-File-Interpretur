2ec6e1fdc052986157fb3715c36b378251d5ddfe
SRGTxTwinkie <zanedreisbig@yahoo.com>
2019-11-24 17:26:50 -0500

    Cock and Balls

---
+++ Interpreter.rb
@@ -0,0 +1,16 @@
+class Interpreter
+
+  def initialize(printOut)
+    puts printOut.class
+    puts printOut.count
+    x = 1
+    File.new("Output.txt", "w")
+    outFile = File.open("Output.txt", "a")
+    for i in printOut
+      outFile.puts("#{x} = #{i}")
+      x += 1
+    end
+    outFile.close
+  end
+
+end
---
+++ TestCase.rb
@@ -0,0 +1,5 @@
+require_relative "Interpreter.rb"
+
+names = File.read("NameList.txt").split(/\n/)
+
+Interpreter.new(names)
